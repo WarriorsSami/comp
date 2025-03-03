4fa25a149d9f8297169657108022cbb0759a80de
WarriorsSami <sbarbutdica@gmail.com>
2025-03-03 22:58:28 +0200

    feat: start working on nested list comp

--- comp_derive/src/lib.rs
+++ comp_derive/src/lib.rs
@@ -1,6 +1,6 @@
 // List comprehension grammar:
 //
-// comp: mapping for_if_clause
+// comp: mapping for_if_clause+
 //
 // mapping: expression
 //
@@ -10,20 +10,30 @@
 // pattern: name (, name)*
 
 use proc_macro2::TokenStream as TokenStream2;
-use quote::{ToTokens, quote};
+use quote::{quote, ToTokens};
 use syn::parse::{Parse, ParseStream};
 use syn::parse_macro_input;
+use vec1::Vec1;
 
+// [[1, 2], [3, 4], [5, 6]] => [1, 2, 3, 4, 5, 6]
+// x for row in matrix for x in row
+
+// [[0, 2], [0, 2], [0, 2]]
+// x for row in 0..3 for x in 0..3 if x % 2 == 0
 struct Comp {
     mapping: Mapping,
-    for_if_clause: ForIfClause,
+    for_if_clauses: Vec1<ForIfClause>,
 }
 
 impl Parse for Comp {
     fn parse(input: ParseStream) -> syn::Result<Self> {
+        let mapping = input.parse()?;
+        let for_if_clauses = Vec1::try_from_vec(parse_zero_or_more(input))
+            .map_err(|_| syn::Error::new(input.span(), "at least one for-if clause is required"))?;
+
         Ok(Self {
-            mapping: input.parse()?,
-            for_if_clause: input.parse()?,
+            mapping,
+            for_if_clauses,
         })
     }
 }
@@ -31,17 +41,15 @@ impl Parse for Comp {
 impl ToTokens for Comp {
     fn to_tokens(&self, tokens: &mut TokenStream2) {
         let Mapping(mapping) = &self.mapping;
-        let ForIfClause {
-            pattern,
+        // TODO: Implement the list comprehension for nested for-if clauses
+        let for_if_clauses = &self.for_if_clauses;
+        
+        let ForIfClause{
             sequence,
+            pattern,
             conditions,
-        } = &self.for_if_clause;
-
-        let conditions = conditions.iter().map(|c| {
-            let Condition(expr) = c;
-            quote! { #expr }
-        });
-
+        } = &for_if_clauses[0];
+        
         tokens.extend(quote! {
             ::core::iter::IntoIterator::into_iter(#sequence).filter_map(|#pattern| {
                 (true #(&& (#conditions))*).then(|| #mapping)
@@ -114,6 +122,12 @@ impl Parse for Condition {
     fn parse(input: ParseStream) -> syn::Result<Self> {
         _ = input.parse::<syn::Token![if]>()?;
         input.parse().map(Self)
+    }
+}
+
+impl ToTokens for Condition {
+    fn to_tokens(&self, tokens: &mut TokenStream2) {
+        self.0.to_tokens(tokens)
     }
 }
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ mod tests {
 
     #[test]
     fn test_list_comprehension() {
-        let result = comp![x * x for x in 0..=10 if x % 2 == 0].collect::<Vec<_>>();
-        assert_eq!(result, vec![0, 4, 16, 36, 64, 100]);
+        let result = comp![x * x for x in 0..=10 if x % 2 == 0 if x % 3 == 0].collect::<Vec<_>>();
+        assert_eq!(result, vec![0, 36]);
     }
 }
