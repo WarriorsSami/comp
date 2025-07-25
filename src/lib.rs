3882d870b237d42177791736cb7b1b6630526406
Bărbuț-Dică Sami <70215066+WarriorsSami@users.noreply.github.com>
2025-07-25 13:24:15 +0300

    test: add nested for-if test

--- src/lib.rs
+++ src/lib.rs
@@ -7,4 +7,13 @@ mod tests {
         let result = comp![x * x for x in 0..=10 if x % 2 == 0 if x % 3 == 0].collect::<Vec<_>>();
         assert_eq!(result, vec![0, 36]);
     }
+
+    #[test]
+    fn test_nested_for_if() {
+        let result = comp![(x, y)
+            for x in 0..=3 if x % 2 == 0
+            for y in 0..=3 if y > x]
+            .collect::<Vec<_>>();
+        assert_eq!(result, vec![(0, 1), (0, 2), (0, 3), (2, 3)]);
+    }
 }
